9e6068e41ba82bdaa35492a9eac7223901ad6aeb
chrisssxuu <chris01.xu@gmail.com>
2018-08-15 14:13:11 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
 class Model
-  # Replace with Dog clas
+  # Replace with Dog class
+  def initialize(name, breed, age)
+    @name=name
+    @breed=breed
+    @age=age
+  end
+  def name
+  end
+  def breed 
+  end
+  def age 
+  end
 end
